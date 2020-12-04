3e6c2fd7e4b7d1aaa8c1be657b6aba34dca5ac98
John Pohovey <jpohovey@gmail.com>
2020-12-03 20:26:58 -0600

    Create file reader functions

---
+++ Code/file_bot.hpp
@@ -0,0 +1,38 @@
+
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+/**
+ * Takes a filename and reads in all the text from the file
+ * Newline characters are also just characters in ASCII
+ * 
+ * @param filename The name of the file that will fill the string
+ */
+std::string file_to_string(const std::string& filename){
+  std::ifstream text(filename);
+
+  std::stringstream strStream;
+  if (text.is_open()) {
+    strStream << text.rdbuf();
+  }
+  return strStream.str();
+}
+
+std::vector<std::string> file_to_vector(const std::string & filename) {
+	std::ifstream text(filename);
+	std::vector<std::string> out;
+
+	if (text.is_open()) {
+		std::istream_iterator<std::string> iter(text);
+		while (!text.eof()) {
+			out.push_back(*iter);
+			++iter;
+		}
+	}
+
+	return out;
+} 
