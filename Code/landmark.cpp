ef75f79b198bfaeea87259e2b60fac502fcd744b
Arjun Ray <arjunr4@illinois.edu>
2020-12-07 00:42:46 -0600

    Landmark Path Added

--- Code/landmark.cpp
+++ Code/landmark.cpp
@@ -1,1 +1,36 @@
+#include "landmark.h"
+#include <queue>
+#include <map>
 
+Landmark::Landmark() : tree_(true, false) {
+    //nothing 
+}
+
+std::map<Vertex, Vertex> Landmark::spanning(const Graph & graph, Vertex v){
+    
+    std::queue<Vertex> q;
+    tree_ = graph;
+
+    std::map<Vertex, bool> visited;
+    std::map<Vertex, Vertex> predecessor;
+    for(Vertex u : tree_.getVertices()){
+        visited[u] = false;
+        predecessor[u] = "0";
+    }
+
+    visited[v] = true;
+    q.push(v);
+
+    while(!q.empty()){
+        Vertex w = q.front();
+        q.pop();
+        for(Vertex y : tree_.getAdjacent(w)){
+            if (visited[y] == false){
+                visited[y] = true;
+                predecessor[y] = w;
+            }
+        }
+    }
+
+    return predecessor;
+}
--- Code/landmark.h
+++ Code/landmark.h
@@ -1,1 +1,21 @@
+#pragma once
+#include <string>
+#include "edge.h"
+#include "graph.h"
+#include "dsets.h"
+#include <vector>
+#include <map>
+#include <algorithm>
+#include "file_bot.hpp"
 
+typedef std::string Vertex;
+
+class Landmark {
+    public:
+        Landmark();
+        std::map<Vertex, Vertex> spanning(const Graph & graph, Vertex v);
+
+    private:
+        Graph tree_;
+
+};
