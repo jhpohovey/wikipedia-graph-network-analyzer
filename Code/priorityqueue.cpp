be936fe12390d75093aeb719d9f9be16546d4342
John Pohovey <jpohovey@gmail.com>
2020-12-07 03:22:37 -0600

    add custom PriorityQueue class for use with Dijkstra's

---
+++ Code/priorityqueue.cpp
@@ -0,0 +1,38 @@
+#include "priorityqueue.h"
+
+PriorityQueue::PriorityQueue() {
+    //do nothing
+}
+
+PriorityQueue::PriorityQueue(Vertex& s) {
+    pq.push(std::make_pair(s, 0));
+}
+
+/*
+ * Since unable to edit, add duplicates to priority queue, but will only deal with one with lowest cost
+ */
+void PriorityQueue::push(std::pair<Vertex, int> p) {
+    pq.push(p);
+}
+
+/*
+ * needs to only deal with min dist for each vertex
+ */
+std::pair<Vertex, int> PriorityQueue::pop() { //this function is questionable in terms of functionality
+    std::pair<Vertex, int> t = pq.top();
+    pq.pop(); //questionable?
+    return t;
+}
+
+std::pair<Vertex, int> PriorityQueue::top() {
+    return pq.top();
+}
+
+size_t PriorityQueue::size() {
+    return pq.size();
+}
+
+bool PriorityQueue::empty() {
+    return pq.empty();
+}
+
---
+++ Code/priorityqueue.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <queue>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+
+typedef std::string Vertex;
+
+class Compare {
+    public:
+        int operator() (const std::pair<Vertex, int>& i, const std::pair<Vertex, int>& j) {
+            return i.second > j.second;
+        }
+};
+
+class PriorityQueue {
+    public:
+        PriorityQueue();
+        PriorityQueue(Vertex& s);
+        void push(std::pair<Vertex, int> p);
+        std::pair<Vertex, int> pop();
+        std::pair<Vertex, int> top();
+        size_t size();
+        bool empty();
+    private:
+        std::priority_queue<std::pair<Vertex, int>, std::vector<std::pair<Vertex, int>>, Compare> pq;
+};
