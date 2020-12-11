62337e8211f16958a7f1991e62558144b37c238b
John Pohovey <jpohovey@gmail.com>
2020-12-11 00:58:35 -0600

    Graph Visualizer organization update and comments

--- Code/graphvis.cpp
+++ Code/graphvis.cpp
@@ -1,5 +1,12 @@
 #include "graphvis.h"
 
+/**
+ * @brief Constructor
+ * 
+ * @param nb, a NetworkBuilder object, which includes both a constructed graph and a frequency list (implemented as an unordered map)
+ * @param widthPNG, the width of the png to visualize the graph onto
+ * @param heightPNG, the height of the png to visualize the graph onto
+ */
 GraphVisualizer::GraphVisualizer(const NetworkBuilder& nb, unsigned widthPNG, unsigned heightPNG) 
         : G(true, false), png(widthPNG, heightPNG) {
     
@@ -10,22 +17,34 @@ GraphVisualizer::GraphVisualizer(const NetworkBuilder& nb, unsigned widthPNG, un
     V = nb.getGraph().getVertices();
     E = nb.getGraph().getEdges();
     area = width * height;
-    k_2 = area / V.size();
+    k_2 = area / V.size(); //constant used for force calculation
     k = sqrt(k_2);
 
-    initRotational();
+    initRotational(); //initialize all vertices in a circular fashion around PNG center
 }
 
+/**
+ * @brief Function computes attractive forces between vertices as a function of their x position
+ * 
+ * @param x, distance between two vertices
+ * @return double, the force experienced at given distance
+ */
 double GraphVisualizer::attractiveForces (double x) {
     return (x * x / k);
 }
 
+/**
+ * @brief Function computes repulsive forces between vertices as a function of their x position 
+ * 
+ * @param x, distance between two vertices
+ * @return double, the force experienced at given distance
+ */
 double GraphVisualizer::repulsiveForces (double x) {
     return (k_2 / x);
 }
 
 /**
- * @brief Implementation of theme and variation of Fruchterman-Reingold Algorithm
+ * @brief Implementation of Fruchterman-Reingold Algorithm (in depth anaylsis linked below)
  * http://cs.brown.edu/people/rtamassi/gdhandbook/chapters/force-directed.pdf
  * 
  * @param iterations, used to calculate 'temperature' decrease for most optimal configuration
@@ -33,51 +52,46 @@ double GraphVisualizer::repulsiveForces (double x) {
  */
 void GraphVisualizer::FruchtermanReingold(size_t iterations) {
 
-    double temperature = 1;
+    double temperature = 1; //temperature controls the spread of point displacement over iterations
     for (size_t i = 0; i < iterations; ++i) {
-        temperature *= (1 / iterations);
         for(auto& v : vnTable) {
+            //calculate all repulsive forces between vertices
             v.second.disp.x = 0;
             v.second.disp.y = 0;
             for (auto& u : vnTable) {
                 if ((u.first != v.first) && 
-                    (u.second.pos.x != v.second.pos.x) && (u.second.pos.y != v.second.pos.y) /*&& 
-                    (u.second.disp.x != v.second.disp.x) && (u.second.disp.y != v.second.disp.y)*/) {
+                    (u.second.pos.x != v.second.pos.x) && (u.second.pos.y != v.second.pos.y)) { //run only if vectors or distinct from one another
                     PositionVector delta = vectorPositionSubtration(v.second, u.second);
-                    if (delta.x < .0001 || delta.y < .0001) {
+
+                    if (delta.x < .0001 || delta.y < .0001) { //edge guard against division by 0
                         delta.x = (rand() % (unsigned int)(.5 * width)) - .5 * width;
                         delta.y = (rand() % (unsigned int)(.5 * height)) - .5 * height;
                     }
-                    //std::cout << "Vertex delta: x: " << delta.x << ", y: " << delta.y << std::endl;
                     double mag = positionVectorMagnitude(delta);
-                    //std::cout << "Magnitude of Delta:" << mag << std::endl;
-                    v.second.disp.x = v.second.disp.x + ((delta.x / mag) * repulsiveForces(mag));
+
+                    v.second.disp.x = v.second.disp.x + ((delta.x / mag) * repulsiveForces(mag)); //updates displacements based on calculated force
                     v.second.disp.y = v.second.disp.y + ((delta.y / mag) * repulsiveForces(mag));
-                    //std::cout << "Dispx: " << v.second.disp.x << ", Dispy: " <<v.second.disp.y << std::endl << std::endl;
                 }
             }
         }
         for (auto& e : E) {
+            //calculates all attractive forces between vertices
             PositionVector delta;
             auto vlookup = vnTable.find(e.source);
             auto ulookup = vnTable.find(e.dest);
             if (vlookup != vnTable.end() && ulookup != vnTable.end()) {
-                /*delta.x = vlookup->second.pos.x - ulookup->second.pos.x;
-                delta.y = vlookup->second.pos.y - ulookup->second.pos.y;*/
                 delta = vectorPositionSubtration(vlookup->second, ulookup->second);
-                //std::cout << std::endl << "Edge delta: x: " << delta.x << ", y: " << delta.y << std::endl;
-                if (delta.x < .0001 || delta.y < .0001) {
+
+                if (delta.x < .0001 || delta.y < .0001) { 
                     delta.x = (rand() % (unsigned int)(.5 * width)) - .5 * width;
                     delta.y = (rand() % (unsigned int)(.5 * height)) - .5 * height;
                 }
                 double mag = positionVectorMagnitude(delta);
-                //std::cout << "Magnitude of Delta:" << mag << std::endl;
-                vlookup->second.disp.x = vlookup->second.disp.x - ((delta.x / mag) * attractiveForces(mag));
+
+                vlookup->second.disp.x = vlookup->second.disp.x - ((delta.x / mag) * attractiveForces(mag)); //updates displacements based on calculated force
                 vlookup->second.disp.y = vlookup->second.disp.y - ((delta.y / mag) * attractiveForces(mag));
-                //std::cout << "v Dispx: " << vlookup->second.disp.x << ", v Dispy: " << vlookup->second.disp.y << std::endl << std::endl;
                 ulookup->second.disp.x = ulookup->second.disp.x + ((delta.x / mag) * attractiveForces(mag));
                 ulookup->second.disp.y = ulookup->second.disp.y + ((delta.y / mag) * attractiveForces(mag));
-                //std::cout << "u Dispx: " << ulookup->second.disp.x << ", u Dispy: " << ulookup->second.disp.y << std::endl << std::endl;
             }
             else {
                 std::cout << "vnTable error" << std::endl;
@@ -86,25 +100,22 @@ void GraphVisualizer::FruchtermanReingold(size_t iterations) {
         }
 
         for(auto& v : vnTable) {
+            //constrains the displacement values as a function of temperature
             double mag = displacementVectorMagnitude(v.second.disp);
-            //std::cout << "before: " << v.second.pos.x << ", " << v.second.pos.y;
             v.second.pos.x = v.second.pos.x + ((v.second.disp.x / mag) * std::min(v.second.disp.x, temperature));
             v.second.pos.y = v.second.pos.y + ((v.second.disp.y / mag) * std::min(v.second.disp.y, temperature));
-            //std::cout <<", mid: " << v.second.pos.x << ", " << v.second.pos.y;
             v.second.pos.x = std::min((double)(width / 2), std::max((double)(width / 2), v.second.pos.x));
             v.second.pos.y = std::min((double)(height / 2), std::max((double)(height / 2), v.second.pos.y));
-            //std::cout << "Possible: max " << (double)(height / 2) << ", " << v.second.pos.y;
-            //std::cout << "\tPossible: min " << (double)(height / 2) << ", " << std::max((double)(height / 2), v.second.pos.y) << std::endl;
-            //std::cout <<", after: " << v.second.pos.x << ", " << v.second.pos.y << std::endl;
         }
+        temperature *= (1 / iterations); //'cool' temperature to relax displacement calculation margins 
     }
 }
 
 /**
- * @brief Plots the graph onto a PNG using a variation of Fruchterman-Reingold Algorithm
+ * @brief Function plots the graph onto a PNG using a variation of Fruchterman-Reingold Algorithm
  * 
  * @param shape, boolean, true to draw vertices as circles, false to draw as squares
- * @return cs225::PNG 
+ * @return cs225::PNG, graph drawn with all vertices and edges
  */
 cs225::PNG GraphVisualizer::drawGraph(bool shape) {
 
@@ -119,57 +130,44 @@ cs225::PNG GraphVisualizer::drawGraph(bool shape) {
     for (auto it = vnTable.begin(); it != vnTable.end(); ++it) {
         Vertex v = it->first;
 
-        Node& n = it->second;
+        Node& n = it->second; //get all components to calculate final x and y positions
         double xpos = n.pos.x;
         double ypos = n.pos.y;
         double xdisp = n.disp.x;
-        double ydisp = n.disp.y;
+        double ydisp = n.disp.y; 
 
-        //std::cout << "Xpos:" << xpos << ", Xdisp: " << xdisp << std::endl;
-        //std::cout << "Ypos:" << ypos << ", Ydisp: " << ydisp << std::endl << std::endl;
-        unsigned xfin = (unsigned int) (std::abs(xpos + xdisp)) % (unsigned int) (.9 * width) + (.05 * width);
-        unsigned yfin = (unsigned int) (std::abs(ypos + ydisp)) % (unsigned int) (.9 * height) + (.05 * height);
-        //std::cout << n.pos.x << "Before " << n.pos.y << std::endl;
-        //std::cout << it->second.pos.x << " Before " << it->second.pos.y << std::endl << std::endl;
-        n.pos.x = xfin;
+        unsigned xfin = (unsigned int) (std::abs(xpos + xdisp)) % (unsigned int) (.9 * width) + (.05 * width); //calculates final resting position
+        unsigned yfin = (unsigned int) (std::abs(ypos + ydisp)) % (unsigned int) (.9 * height) + (.05 * height); //add a small border on all edges of PNG
+
+        n.pos.x = xfin; //update node positions for use in drawing edges
         n.pos.y = yfin;
     }
-        //std::cout << n.pos.x << " After" << n.pos.y << std::endl << std::endl;
-        //std::cout << it->second.pos.x << " After " << it->second.pos.y << std::endl << std::endl;
-    drawEdges();
-        //unsigned xfin = std::abs(xpos);
-        //unsigned yfin = std::abs(ypos);
-        //xfin = (xfin + rand() % width) % width;
-        //yfin = (yfin + rand() % height) % height;
-    for (auto it = vnTable.begin(); it != vnTable.end(); ++it) {
-        Vertex v = it->first;
 
-        Node& n = it->second;
-        unsigned xfin = n.pos.x;
+    //draw edges after calculating the final resting coordinates of all vertices, but before drawing vertices
+    // so that edges are drawn underneath vertices.
+    drawEdges(); 
+
+    for (auto it = vnTable.begin(); it != vnTable.end(); ++it) {
+        Vertex v = it->first; 
+
+        Node& n = it->second; 
+        unsigned xfin = n.pos.x; //get final x,y coordinates
         unsigned yfin = n.pos.y;
 
-        auto lookup = freqList.find(v);
+        auto lookup = freqList.find(v); //lookup Vertex, frequency pair to get vertex's occurrence in file
         if (lookup == freqList.end()) {
             std::cout << "Frequency hashmap error" << std::endl;
             abort();
         }
-        int freq = lookup->second;
+        int freq = lookup->second; 
 
-        cs225::HSLAPixel color = colors[rand() % colors.size()];
-        //std::cout << "Freq" << freq << std::endl;
-        double factor  = .5;
+        cs225::HSLAPixel color = colors[rand() % colors.size()]; //choose a random color for drawing vertex 
+        double factor  = .5; //determines how much of the full vertex will be drawn onto png
         for (unsigned int i = xfin - std::ceil(factor * freq); i < xfin + std::ceil(factor * freq); ++i) {
             for (unsigned int j = yfin - std::ceil(factor * freq); j < yfin + std::ceil(factor * freq); ++j) {
-                //std::cout << "Pixel - " << i << ", " << j << std::endl;
-                /*std::cout << "xfin - .25 * 20: " << xfin - .25 * 20 << ", xfin + .25 * 20: " << xfin + .25 * 20 << std::endl;
-                std::cout << "yfin - .25 * 20: " << yfin - .25 * 20 << ", yfin + .25 * 20: " << yfin + .25 * 20 << std::endl;*/
-                //std::cout << "i: " << i << ", j: " << j << std::endl;
-                //std::cout << "xfin " << xfin << ", yfin " << yfin << std::endl;
-                //std::cout << "Pythag: " << (xfin - i) * (xfin - i) + (yfin - j) * (yfin - j) << std::endl;
                 if (i < width && j < height) {
-                    if (shape) {
-                        if (((xfin - i) * (xfin - i) + (yfin - j) * (yfin - j) <= .25 * freq * freq)) {
-                        //std::cout << "\tPixel - " << i << ", " << j << std::endl;
+                    if (shape) { //determines if vertices will be square or circular in shape based on passed param
+                        if (((xfin - i) * (xfin - i) + (yfin - j) * (yfin - j) <= .25 * freq * freq)) { //only colors pixels within the radius of the center
                             cs225::HSLAPixel& pixel = png.getPixel(i, j);
                             pixel.h = color.h;
                             pixel.l = color.l;
@@ -193,6 +191,10 @@ cs225::PNG GraphVisualizer::drawGraph(bool shape) {
     return png;
 }
 
+/**
+ * @brief Function draw all edges between all vertices (in black)
+ * 
+ */
 void GraphVisualizer::drawEdges() {
     for (auto& e : E) {
         auto ulookup = vnTable.find(e.source);
@@ -207,18 +209,26 @@ void GraphVisualizer::drawEdges() {
     }
 }
 
+/**
+ * @brief Function is a helper function for drawEdge. Calculates slope and plots 
+ *        line between the two coordinate params.
+ * 
+ * @param x1, X component of first coordinate
+ * @param x2, X component of second coordinate
+ * @param y1, Y component of first coordinate
+ * @param y2, Y component of second coordinate
+ */
 void GraphVisualizer::drawLine(double x1, double x2, double y1, double y2) {
     cs225::HSLAPixel black(0, 1, 0, 1);
-    //std::cout << "x1 " << x1 << ", x2 " << x2 << ", y1 " << y1 << ", y2 " << y2<<std::endl;
 
     double slope;
     if ((std::abs(y1 - y2) < .001) && std::abs(x1 - x2) < .1) {
-        //same point / close enough, do nothing
+        //coordinates are the same point / are close enough, do nothing
         return;
     }
-    else if (std::abs(y1 - y2) < .001) {
+    else if (std::abs(y1 - y2) < .001) { // if same y values, can just increment x values (no slope needed).
         if (x1 > x2) {
-            for (size_t i = x1; i > x2; --i) {
+            for (size_t i = x1; i > x2; --i) { //direction of iteration depends on which component is greater
                 cs225::HSLAPixel& pixel = png.getPixel(i, y1);
                 pixel.l = black.l;
             }
@@ -230,7 +240,7 @@ void GraphVisualizer::drawLine(double x1, double x2, double y1, double y2) {
             }
         }
     }
-    else if (std::abs(x1 - x2) < .1) {
+    else if (std::abs(x1 - x2) < .1) { // if same x values, can just increment y values (no slope needed).
         if (y1 > y2) {
             for (size_t j = y1; j > y2; --j) {
                 cs225::HSLAPixel& pixel = png.getPixel(x1, j);
@@ -246,71 +256,22 @@ void GraphVisualizer::drawLine(double x1, double x2, double y1, double y2) {
     }
     else {
         slope = (y2 - y1) / (x2 - x1);
-        /*if (x1 > x2) {
-            if (y1 > y2) {
-                for (size_t i = x1; i > x2; --i) {
-                    for (size_t j = y1; j > y2; --j) {
-                        if (std::abs(j - (slope * (i - x1) + y1)) < 2) {
-                            cs225::HSLAPixel& pixel = png.getPixel(x1, j);
-                            pixel.l = black.l;
-                        }
-                    }
-                }
-            }
-            else {
-                for (size_t i = x1; i > x2; --i) {
-                    for (size_t j = y1; j < y2; ++j) {
-                        if (std::abs(j - (slope * (i - x1) + y1)) < 2) {
-                            cs225::HSLAPixel& pixel = png.getPixel(x1, j);
-                            pixel.l = black.l;
-                        }
-                    }
-                }
-            }
-        }
-        else {
-            if (y1 > y2) {
-                for (size_t i = x1; i < x2; ++i) {
-                    for (size_t j = y1; j > y2; --j) {
-                        if (std::abs(j - (slope * (i - x1) + y1)) < 2) {
-                            cs225::HSLAPixel& pixel = png.getPixel(x1, j);
-                            pixel.l = black.l;
-                        }
-                    }
-                }
-            }
-            else {
-                for (size_t i = x1; i < x2; ++i) {
-                    for (size_t j = y1; j < y2; ++j) {
-                        if (std::abs(j - (slope * (i - x1) + y1)) < 2) {
-                            cs225::HSLAPixel& pixel = png.getPixel(x1, j);
-                            pixel.l = black.l;
-                        }
-                    }
-                }
-            }
-        }*/
-        
         if (x1 > x2) {
             double x = x2;
-            //std::cout << x1 << " " << x2 << std::endl;
             while (x < x1) {
                 int y = slope * (x - x1) + y1;
                 cs225::HSLAPixel& pixel = png.getPixel(x, y);
                 pixel.l = black.l;
                 x+=2;
-                //std::cout << x << std::endl;
             }
         }
         else {
             double x = x1;
-            //std::cout << x1 << " " << x2 << std::endl;
             while (x < x2) {
                 int y = slope * (x - x2) + y2;
                 cs225::HSLAPixel& pixel = png.getPixel(x, y);
                 pixel.l = black.l;
                 x+=2;
-                //std::cout << x << std::endl;
             }
         }
     }
@@ -325,21 +286,23 @@ void GraphVisualizer::drawLine(double x1, double x2, double y1, double y2) {
 void GraphVisualizer::initRotational() {
     const double pi = M_PI;
     double dRad = (double) 1 / (double) V.size();
-    //printf("%lu\n", V.size());
     double rot = 0;
     for (size_t i = 0; i < V.size(); ++i) {
         Node n;
         n.pos.x = .5 * width + .25 * width * cos(rot);
         n.pos.y = .5 * height + .25 * height * sin(rot);
-        //std::cout << "Vertex: " << V[i] << ", Xpos: " << n.pos.x << ", Ypos: " << n.pos.y << std::endl;
         vnTable.insert(std::make_pair(V[i], n));
         rot += (2 * pi * dRad);
-        //printf("rot: %f\n", rot);
     }
-
-    
 }
 
+/**
+ * @brief Function performs vector subtraction
+ * 
+ * @param a, Minuend vector for subtraction operation
+ * @param b, subtrahend vector for subtraction operation
+ * @return PositionVector, which is the resulting difference vector
+ */
 PositionVector GraphVisualizer::vectorPositionSubtration(const Node& a, const Node& b) {
     PositionVector d;
     d.x = a.pos.x - b.pos.x;
@@ -347,10 +310,22 @@ PositionVector GraphVisualizer::vectorPositionSubtration(const Node& a, const No
     return d;
 }
 
+/**
+ * @brief Function calculates the magnitude of a (position) vector 
+ * 
+ * @param a, the vector whose magnitude will be calculated
+ * @return double, the magnitude of the vector
+ */
 double GraphVisualizer::positionVectorMagnitude(const PositionVector& a) {
     return sqrt((a.x * a.x) + (a.y * a.y));
 }
 
+/**
+ * @brief Function calculates the magnitude of a (displacement) vector 
+ * 
+ * @param a, the vector whose magnitude will be calculated
+ * @return double, the magnitude of the vector
+ */
 double GraphVisualizer::displacementVectorMagnitude(const DisplacementVector& a) {
     return sqrt((a.x * a.x) + (a.y * a.y));
 }
--- Code/graphvis.h
+++ Code/graphvis.h
@@ -1,3 +1,22 @@
+/**
+ * @file dijkstraSSSP.cpp
+ * @author John Pohovey, Arjun Ray
+ * @brief Class for generating a graphical visual of a NetworkBuilder's graph object.
+ *        This graph visualization is a force directed graph, and is an implementation
+ *        of the Fruchterman-Reingold Algorithm (1991). Vertices are initialized with 
+ *        positions that together form a circle (this is aids in optimizing the scattering
+ *        that ocurrs when running the algorithm.). The computations are then drawn 
+ *        onto a PNG-type object.
+ * @version 0.1
+ * 
+ * @date December 2020
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#pragma once
+
 #include <string>
 #include <algorithm>
 #include <cmath>
@@ -10,23 +29,25 @@
 #include "Network.h"
 #include <stdio.h>
 
-typedef struct {
+typedef struct { //struct to represent a vertex's position vector
     double x;
     double y;
 } PositionVector;
 
-typedef struct {
+typedef struct { //struct to represent a vertex's displacement vector
     double x; 
     double y;
 } DisplacementVector;
 
-typedef struct {
+typedef struct { //struct to combine both vectors into a vertex's node
     PositionVector pos;
     DisplacementVector disp;
 } Node;
 
 class GraphVisualizer {
     public:
+        //implementations notes on function, parameters, returns, etc are all located in the .cpp file
+
         GraphVisualizer(const NetworkBuilder& nb, unsigned widthPNG, unsigned heightPNG);
         void FruchtermanReingold(size_t iterations);
         cs225::PNG drawGraph(bool shape);
@@ -43,13 +64,12 @@ class GraphVisualizer {
         void drawEdges();
         void drawLine(double x1, double x2, double y1, double y2);
 
-        double k;
-        double k_2;
-
         cs225::PNG png;
         unsigned width;
         unsigned height;
         double area;
+        double k;
+        double k_2;
         
         std::vector<Vertex> V;
         std::vector<Edge> E;
